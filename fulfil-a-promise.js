fe9ddad4121dc2904b9f05a3a311a562f9d6fff9
Daniel Zwelling <dzwellingmusic@gmail.com>
2017-01-22 16:59:52 -0800

    Exercise deux

---
+++ fulfil-a-promise.js
@@ -0,0 +1,32 @@
+'use stric';
+
+const newPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('FULFILLED!');
+  }, 300);
+});
+
+newPromise.then(console.log);
+
+
+// Their solution
+
+'use strict';
+
+// Create a promise
+
+var promise = new Promise(function (fulfill, reject) {
+  // After the timeout reaches 300ms, fulfill the promise with value
+  // 'FULFILLED!'.
+
+  setTimeout(function () {
+    fulfill('FULFILLED!');
+  }, 300);
+});
+
+// Add a handler to the promise’s fulfillment. `console.log` will be called
+// with the value passed to `fulfill`, which is `'FULFILLED!'`.
+// Note that this statement will ALWAYS be executed before `fulfill` is
+// called (we'll talk about this in depth in the lessons to come).
+
+promise.then(console.log);

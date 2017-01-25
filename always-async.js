6ead93739bce61ce2609c4a4f7dfed5038aaf237
Daniel Zwelling <dzwellingmusic@gmail.com>
2017-01-25 14:47:54 -0800

    Exercise five

---
+++ always-async.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const promise = new Promise((resolve, reject) => {
+  resolve('PROMISE VALUE');
+});
+
+promise.then(console.log);
+
+console.log('MAIN PROGRAM');
+
+
+// Their solution
+
+'use strict';
+
+var promise = new Promise(function (fulfill, reject) {
+  fulfill('PROMISE VALUE');
+});
+
+// At this point, the value of promise is already known.
+
+// If promise is not always asynchronous, console.log would be called with
+// 'PROMISE VALUE' here. This is not the case.
+
+promise.then(console.log);
+
+console.log('MAIN PROGRAM');
